c4a4c12e6462acbbb81c713e4cdfa5e39b12fe6e
dyegoreisa <dyegoreisa@f99c5bf0-686d-9267-5977-06f448c201e9>
2010-12-28 17:43:19 +0000

    Retirada dos caracteres \t e \n

--- trunk/backup-thunderbird.sh
+++ trunk/backup-thunderbird.sh
@@ -10,53 +10,53 @@ ANOMESDIA=`date +%Y%m%d`
 AGORA=`date`
 
 echo "Inicio do backup do dia "$AGORA;
-echo "\t\tMontando compartinhamento...";
+echo "  Montando compartinhamento...";
 smbmount $SERVER $DIRBK -o credentials=$CREDENTIALS
 
 if ls $DIRBK 1> /dev/null 2> /dev/null;
 then
-    echo "\t\tCompartinhamento montado";
+    echo "  Compartinhamento montado";
 else 
     echo "[ERRO] - Erro na montagem do compartilhamento"
     exit 1;
 fi;
 
-echo "\t\tEntrando em "$DIRBK"...";
+echo "  Entrando em "$DIRBK"...";
 cd $DIRBK
-echo "\t\tDentro de "$DIRBK;
+echo "  Dentro de "$DIRBK;
 
 echo "Verificando existencia do diretório "$ANOMES"...";
 if ls $ANOMES 1> /dev/null 2> /dev/null; 
 then 
     cd $ANOMES;
-    echo "\t\tDentro do diretório "$ANOMES;
+    echo "  Dentro do diretório "$ANOMES;
 else 
-    echo "\t\tCriando diretório "$ANOMES"...";
+    echo "  Criando diretório "$ANOMES"...";
     mkdir $ANOMES;
     cd $ANOMES;
-    echo "\t\tDentro do diretório "$ANOMES;
+    echo "  Dentro do diretório "$ANOMES;
 fi;
 
 # backup do sistema
-echo "\t\tEntrando no diretório "$SYSDIR;
+echo "  Entrando no diretório "$SYSDIR;
 cd $SYSDIR
 
-echo "\t\tParando o Thunderbird..."
+echo "  Parando o Thunderbird..."
 killall -s STOP thunderbird-bin
 
-echo "\t\tCriando arquivo de backup...";
+echo "  Criando arquivo de backup...";
 tar -P -czf thunderbird-$ANOMESDIA.tar.gz .thunderbird
 
-echo "\t\tVoltando o Thunderbird..."
+echo "  Voltando o Thunderbird..."
 killall -s CONT thunderbird-bin
 
-echo "\t\tVoltando para o diretório do backup...";
+echo "  Voltando para o diretório do backup...";
 cd - 
 
-echo "\t\tMovendo o arquivo para o diretório de backup...";
+echo "  Movendo o arquivo para o diretório de backup...";
 mv -v $SYSDIR"thunderbird-$ANOMESDIA.tar.gz" . 
 
-echo "\t\tDesmontando o compartilhamento...";
+echo "  Desmontando o compartilhamento...";
 umount -f $DIRBK
 
-echo "Backup feito!\n"
+echo "Backup feito!"
