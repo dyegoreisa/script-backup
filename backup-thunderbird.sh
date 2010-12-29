e4de105ee8fe1e0414b929b8e001191f7c447933
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2010-12-29 18:22:43 +0000

    Correção da forma como estava sendo feito o backup, para ter somente um arquivo por mês

--- backup-thunderbird.sh
+++ backup-thunderbird.sh
@@ -6,14 +6,12 @@ SYSDIR="/home/dyego/"
 CREDENTIALS="/home/dyego/.crendentials"
 
 ANOMES=`date +%Y%m`
-ANOMESDIA=`date +%Y%m%d`
 AGORA=`date`
 
+echo "+-------------------------------------------------------+";
 echo "Inicio do backup do dia "$AGORA;
 echo "  Montando compartinhamento...";
-smbmount $SERVER $DIRBK -o credentials=$CREDENTIALS
-
-if ls $DIRBK 1> /dev/null 2> /dev/null;
+if smbmount $SERVER $DIRBK -o credentials=$CREDENTIALS 1> /dev/null 2> /dev/null;
 then
     echo "  Compartinhamento montado";
 else 
@@ -25,38 +23,28 @@ echo "  Entrando em "$DIRBK"...";
 cd $DIRBK
 echo "  Dentro de "$DIRBK;
 
-echo "Verificando existencia do diretório "$ANOMES"...";
-if ls $ANOMES 1> /dev/null 2> /dev/null; 
-then 
-    cd $ANOMES;
-    echo "  Dentro do diretório "$ANOMES;
-else 
-    echo "  Criando diretório "$ANOMES"...";
-    mkdir $ANOMES;
-    cd $ANOMES;
-    echo "  Dentro do diretório "$ANOMES;
-fi;
-
-# backup do sistema
-echo "  Entrando no diretório "$SYSDIR;
-cd $SYSDIR
-
 echo "  Parando o Thunderbird..."
 killall -s STOP thunderbird-bin
 
 echo "  Criando arquivo de backup...";
-tar -P -czf thunderbird-$ANOMESDIA.tar.gz .thunderbird
+tar -P -czf thunderbird-$ANOMES.tar.gz .thunderbird
 
 echo "  Voltando o Thunderbird..."
 killall -s CONT thunderbird-bin
 
 echo "  Voltando para o diretório do backup...";
-cd - 
+cd - 1> /dev/null 2> /dev/null;
 
 echo "  Movendo o arquivo para o diretório de backup...";
-mv -v $SYSDIR"thunderbird-$ANOMESDIA.tar.gz" . 
+mv -vf $SYSDIR"thunderbird-$ANOMES.tar.gz" . 
 
 echo "  Desmontando o compartilhamento...";
-umount -f $DIRBK
+if umount -f $DIRBK 1> /dev/null 2> /dev/null;
+then
+    echo "  Compartilhamento desmontado";
+else
+    echo "[ERRO] - Não foi possível desmostar o compartilhamento.";
+fi;
 
 echo "Backup feito!"
+echo "+-------------------------------------------------------+";
