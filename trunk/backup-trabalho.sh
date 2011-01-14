cd42789c28af58f9287746f45c5ce4db389b2cee
dyegoreisa <dyegoreisa@f99c5bf0-686d-9267-5977-06f448c201e9>
2011-01-14 14:21:10 +0000

    mudança do nome do script para trabalho

--- trunk/backup-trabalho.sh
+++ trunk/backup-trabalho.sh
@@ -5,11 +5,18 @@ DIRBK="/media/backup"
 SYSDIR="/home/dyego/"
 CREDENTIALS="/home/dyego/.crendentials"
 
+TMP=/tmp/`date +%Y%m%d%s`
 ANOMES=`date +%Y%m`
 AGORA=`date`
 
+TAR="tar -P -czf "
+
 echo "+-------------------------------------------------------+";
 echo "Inicio do backup do dia "$AGORA;
+
+echo "  Criando pasta temporaria...";
+mkdir $TMP
+
 echo "  Montando compartinhamento...";
 if smbmount $SERVER $DIRBK -o credentials=$CREDENTIALS 1> /dev/null 2> /dev/null;
 then
@@ -19,24 +26,33 @@ else
     exit 1;
 fi;
 
-echo "  Entrando em "$DIRBK"...";
-cd $DIRBK
-echo "  Dentro de "$DIRBK;
+echo "  Entrando em "$SYSDIR"...";
+cd $SYSDIR
+echo "  Dentro de "$SYSDIR;
 
 echo "  Parando o Thunderbird..."
 killall -s STOP thunderbird-bin
 
-echo "  Criando arquivo de backup...";
-tar -P -czf thunderbird-$ANOMES.tar.gz .thunderbird
+echo "  Criando arquivo de backup do Thunderbird...";
+$TAR$TMP/thunderbird-$ANOMES.tar.gz .thunderbird
 
 echo "  Voltando o Thunderbird..."
 killall -s CONT thunderbird-bin
 
-echo "  Voltando para o diretório do backup...";
-cd - 1> /dev/null 2> /dev/null;
+echo "  Criando arquivo de backup da pasta Documentos...";
+$TAR$TMP/documentos-$ANOMES.tar.gz Documentos
 
-echo "  Movendo o arquivo para o diretório de backup...";
-mv -vf $SYSDIR"thunderbird-$ANOMES.tar.gz" . 
+echo "  Criando arquivo de backup da pasta SQLs...";
+$TAR$TMP/SQLs-$ANOMES.tar.gz SQLs
+
+echo "  Criando arqivos de backup da pasta Desktop...";
+$TAR$TMP/Desktop-$ANOMES.tar.gz Desktop
+
+echo "  Criando arquivo de backup da parta outros...";
+$TAR$TMP/outros-$ANOMES.tar.gz outros
+
+echo "  Movendo o arquivo do Thunderbird para o diretório de backup...";
+mv -vf $TMP/* $DIRBK 
 
 echo "  Desmontando o compartilhamento...";
 if umount -f $DIRBK 1> /dev/null 2> /dev/null;
